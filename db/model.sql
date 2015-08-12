8ed9928fee4b98d56e6451b90b1d550c352d573b
Michael Steigman <michael.r.steigman@uth.tmc.edu>
2015-08-11 22:43:48 -0400

    added views and summary/descriptive stats generation to model file

--- db/model.sql
+++ db/model.sql
@@ -31,7 +31,7 @@ CREATE VIEW ensemble_class_dictionary_flat_meds AS
        FROM ensemble_class_dictionary
    GROUP BY upper(item_string), label;
 
-
+-- specific to project data
 -- views of each pairs list
 CREATE VIEW cs_ut_pairs AS
      SELECT coalesce(m.label, p.med_string) AS med, coalesce(pr.label, p.problem_string) AS problem
@@ -40,5 +40,114 @@ CREATE VIEW cs_ut_pairs AS
   LEFT JOIN ensemble_class_dictionary_flat_problems pr ON (lower(p.problem_string) = lower(pr.item_string))
       WHERE source_file = 'cs_ut';
 
--- finding crossover
-select count(*), b.med, b.problem from bwh_pairs b join uth_pairs u on b.med = u.med and b.problem = u.problem group by b.med, b.problem;
+CREATE VIEW rep_ut_pairs AS
+     SELECT coalesce(m.label, p.med_string) AS med, coalesce(pr.label, p.problem_string) AS problem
+       FROM ensemble_pairs p
+  LEFT JOIN ensemble_class_dictionary_flat_meds m ON (lower(p.med_string) = lower(m.item_string))
+  LEFT JOIN ensemble_class_dictionary_flat_problems pr ON (lower(p.problem_string) = lower(pr.item_string))
+      WHERE source_file = 'rep_ut';
+
+CREATE VIEW uth_pairs AS
+     SELECT coalesce(m.label, p.med_string) AS med, coalesce(pr.label, p.problem_string) AS problem
+       FROM ensemble_pairs p
+  LEFT JOIN ensemble_class_dictionary_flat_meds m ON (lower(p.med_string) = lower(m.item_string))
+  LEFT JOIN ensemble_class_dictionary_flat_problems pr ON (lower(p.problem_string) = lower(pr.item_string))
+      WHERE source_file = 'uth';
+
+CREATE VIEW bwh_pairs AS
+     SELECT coalesce(m.label, p.med_string) AS med, coalesce(pr.label, p.problem_string) AS problem
+       FROM ensemble_pairs p
+  LEFT JOIN ensemble_class_dictionary_flat_meds m ON (lower(p.med_string) = lower(m.item_string))
+  LEFT JOIN ensemble_class_dictionary_flat_problems pr ON (lower(p.problem_string) = lower(pr.item_string))
+      WHERE source_file = 'bwh';
+
+CREATE VIEW bcbstx_pairs AS
+     SELECT coalesce(m.label, p.med_string) AS med, coalesce(pr.label, p.problem_string) AS problem
+       FROM ensemble_pairs p
+  LEFT JOIN ensemble_class_dictionary_flat_meds m ON (lower(p.med_string) = lower(m.item_string))
+  LEFT JOIN ensemble_class_dictionary_flat_problems pr ON (lower(p.problem_string) = lower(pr.item_string))
+      WHERE source_file = 'bcbstx';
+
+CREATE VIEW orig_bcbstx_pairs AS
+     SELECT med_string, problem_string
+       FROM ensemble_pairs p
+      WHERE source_file = 'bcbstx';
+      
+CREATE VIEW orig_uth_pairs AS
+     SELECT med_string, problem_string
+       FROM ensemble_pairs p
+      WHERE source_file = 'uth';
+
+CREATE VIEW orig_bwh_pairs AS
+     SELECT med_string, problem_string
+       FROM ensemble_pairs p
+      WHERE source_file = 'bwh';
+
+CREATE VIEW orig_rep_ut_pairs AS
+     SELECT med_string, problem_string
+       FROM ensemble_pairs p
+      WHERE source_file = 'rep_ut';
+
+CREATE VIEW orig_cs_ut_pairs AS
+     SELECT med_string, problem_string
+       FROM ensemble_pairs p
+      WHERE source_file = 'cs_ut';
+
+-- SQL to output pairs
+COPY (SELECT COALESCE(m.label, p.med_string) AS med, COALESCE(pr.label, p.problem_string) AS problem
+        FROM ensemble_pairs p
+   LEFT JOIN ensemble_class_dictionary_flat_meds m ON (lower(p.med_string) = lower(m.item_string))
+   LEFT JOIN ensemble_class_dictionary_flat_problems pr ON (lower(p.problem_string) = lower(pr.item_string))
+       WHERE source_file = 'uth'
+    ORDER BY pair_id ASC) TO '/tmp/uth.csv' WITH csv;
+
+COPY (SELECT COALESCE(m.label, p.med_string) AS med, COALESCE(pr.label, p.problem_string) AS problem
+        FROM ensemble_pairs p
+   LEFT JOIN ensemble_class_dictionary_flat_meds m ON (lower(p.med_string) = lower(m.item_string))
+   LEFT JOIN ensemble_class_dictionary_flat_problems pr ON (lower(p.problem_string) = lower(pr.item_string))
+       WHERE source_file = 'bwh'
+    ORDER BY pair_id ASC) TO '/tmp/bwh.csv' WITH csv;
+
+COPY (SELECT COALESCE(m.label, p.med_string) AS med, COALESCE(pr.label, p.problem_string) AS problem
+        FROM ensemble_pairs p
+   LEFT JOIN ensemble_class_dictionary_flat_meds m ON (lower(p.med_string) = lower(m.item_string))
+   LEFT JOIN ensemble_class_dictionary_flat_problems pr ON (lower(p.problem_string) = lower(pr.item_string))
+       WHERE source_file = 'bcbstx'
+    ORDER BY pair_id ASC) TO '/tmp/bcbstx.csv' WITH csv;
+
+COPY (SELECT COALESCE(m.label, p.med_string) AS med, COALESCE(pr.label, p.problem_string) AS problem
+        FROM ensemble_pairs p
+   LEFT JOIN ensemble_class_dictionary_flat_meds m ON (lower(p.med_string) = lower(m.item_string))
+   LEFT JOIN ensemble_class_dictionary_flat_problems pr ON (lower(p.problem_string) = lower(pr.item_string))
+       WHERE source_file = 'rep_ut'
+    ORDER BY pair_id ASC) TO '/tmp/rep_ut.csv' WITH csv;
+
+COPY (SELECT COALESCE(m.label, p.med_string) AS med, COALESCE(pr.label, p.problem_string) AS problem
+        FROM ensemble_pairs p
+   LEFT JOIN ensemble_class_dictionary_flat_meds m ON (lower(p.med_string) = lower(m.item_string))
+   LEFT JOIN ensemble_class_dictionary_flat_problems pr ON (lower(p.problem_string) = lower(pr.item_string))
+       WHERE source_file = 'cs_ut'
+    ORDER BY pair_id ASC) TO '/tmp/cs_ut.csv' WITH csv;
+    
+-- finding overlap
+select count(*), b.med, b.problem from bwh_pairs a join uth_pairs u on b.med = u.med and b.problem = u.problem group by b.med, b.problem;
+
+-- count the overlap between two sources of pairs
+select count(*) from (select 1 from bwh_pairs a join uth_pairs b on a.med = b.med and a.problem = b.problem group by a.med, a.problem) t;
+select count(*) from (select 1 from bwh_pairs a join bcbstx_pairs b on a.med = b.med and a.problem = b.problem group by a.med, a.problem) t;
+select count(*) from (select 1 from bwh_pairs a join rep_ut_pairs b on a.med = b.med and a.problem = b.problem group by a.med, a.problem) t;
+select count(*) from (select 1 from cs_ut_pairs a join bwh_pairs b on a.med = b.med and a.problem = b.problem group by a.med, a.problem) t;
+select count(*) from (select 1 from cs_ut_pairs a join bcbstx_pairs b on a.med = b.med and a.problem = b.problem group by a.med, a.problem) t;
+select count(*) from (select 1 from cs_ut_pairs a join rep_ut_pairs b on a.med = b.med and a.problem = b.problem group by a.med, a.problem) t;
+select count(*) from (select 1 from cs_ut_pairs a join uth_pairs b on a.med = b.med and a.problem = b.problem group by a.med, a.problem) t;
+select count(*) from (select 1 from rep_ut_pairs a join bcbstx_pairs b on a.med = b.med and a.problem = b.problem group by a.med, a.problem) t;
+select count(*) from (select 1 from rep_ut_pairs a join uth_pairs b on a.med = b.med and a.problem = b.problem group by a.med, a.problem) t;
+select count(*) from (select 1 from bcbstx_pairs a join uth_pairs b on a.med = b.med and a.problem = b.problem group by a.med, a.problem) t;
+
+select count(*)
+from uth_pairs a
+join rep_ut_pairs b on a.med = b.med and a.problem = b.problem
+join cs_ut_pairs c on b.med = c.med and b.problem = c.problem
+join bwh_pairs d on c.med = d.med and c.problem = d.problem
+join bcbstx_pairs e on d.med = e.med and d.problem = e.problem;
+group by 1.med, 1.problem;
